9aaf4facbdb111360a52b028eb7f3348dd6ca2a3
ljh <ljhbupt@163.com>
2018-12-17 23:51:35 +0800

    code

--- 1-100/leetcode4.cpp
+++ 1-100/leetcode4.cpp
@@ -1,2 +1,27 @@
-leetcode test
-	
+/*思考：
+需要找到两个数列的中数，而且要想算法时间复杂度为O(log (m+n))，我们首先考虑使用二分法来找两个有序数列的中数，
+首先对着两个数列分别取中位数，比较其大小，然后可以确定要找的中位数的范围，之后排除掉一些数循环操作。
+*/
+class Solution {
+public:
+    int kth(int a[], int m, int b[], int n, int k) {
+        if (m < n) return kth(b,n,a,m,k);
+        if (n==0) return a[k-1];
+        if (k==1) return min(a[0],b[0]);
+
+        int j = min(n,k/2);
+        int i = k-j;
+        if (a[i-1] > b[j-1]) return kth(a,i,b+j,n-j,k-j);
+        return kth(a+i,m-i,b,j,k-i);
+    }
+
+    double findMedianSortedArrays(int a[], int m, int b[], int n) {
+        int k = (m+n)/2;
+        int m1 = kth(a,m,b,n,k+1);
+        if ((m+n)%2==0) {
+            int m2 = kth(a,m,b,n,k);
+            return ((double)m1+m2)/2.0;
+        }
+        return m1;
+    }
+};
