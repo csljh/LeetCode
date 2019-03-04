eb4ad3b3d2fae46c3c452716375c66070d7b44b4
ljh <ljhbupt@163.com>
2019-03-04 09:13:42 +0800

    4

---
+++ 1-100/leetcode24.cpp
@@ -0,0 +1,18 @@
+//递归和迭代都可以
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *dummy = new ListNode(-1), *pre = dummy;
+        dummy->next = head;
+        while (pre->next && pre->next->next) {
+            ListNode *t = pre->next->next;
+            pre->next->next = t->next;
+            t->next = pre->next;
+            pre->next = t;
+            pre = t->next;
+        }
+        return dummy->next;
+    }
+};
+
---
+++ 1-100/leetcode27.cpp
@@ -0,0 +1,11 @@
+//±éÀú£¬¼ÆÊý£¬É¾³ý
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != val) nums[res++] = nums[i];
+        }
+        return res;
+    }
+};
