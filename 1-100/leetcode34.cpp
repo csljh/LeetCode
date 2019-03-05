8f5d071adc522e8f91828ecaf5ed14e82166dcfc
ljh <ljhbupt@163.com>
2019-03-05 14:51:27 +0800

    5

---
+++ 1-100/leetcode34.cpp
@@ -0,0 +1,21 @@
+//首先对原数组使用二分查找法，找出其中一个目标值的位置，然后向两边搜索找出起始和结束的位置
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int idx = search(nums, 0, nums.size() - 1, target);
+        if (idx == -1) return {-1, -1};
+        int left = idx, right = idx;
+        while (left > 0 && nums[left - 1] == nums[idx]) --left;
+        while (right < nums.size() - 1 && nums[right + 1] == nums[idx]) ++right;
+        return {left, right};
+    }
+    int search(vector<int>& nums, int left, int right, int target) {
+        if (left > right) return -1;
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+        else if (nums[mid] < target) return search(nums, mid + 1, right, target);
+        else return search(nums, left, mid - 1, target);
+    }
+};
+
---
+++ 1-100/leetcode39.cpp
@@ -0,0 +1,19 @@
+//ตÝน้
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int> &candidates, int target) {
+        vector<vector<int>> res;
+        combinationSumDFS(candidates, target, 0, {}, res);
+        return res;
+    }
+    void combinationSumDFS(vector<int> &candidates, int target, int start, vector<int> out, vector<vector<int>> &res) {
+        if (target < 0) return;
+        if (target == 0) {res.push_back(out); return;}
+        for (int i = start; i < candidates.size(); ++i) {
+            out.push_back(candidates[i]);
+            combinationSumDFS(candidates, target - candidates[i], i, out, res);
+            out.pop_back();
+        }
+    }
+};
